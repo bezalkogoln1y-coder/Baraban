a6e393a75fb0bb1d850b5afaa041ff84f1c2053e
homePC <muskatinartem@gmail.com>
2020-09-21 22:16:37 +0300

    release 0.0.1

--- src/Classes/ParserRate.php
+++ src/Classes/ParserRate.php
@@ -15,7 +15,7 @@ class ParserRate implements ClassInterface {
          $this->config = $customConfig;
       } else {
          $this->config = [
-            'urlDonor' => 'https://www.coingecko.com/ru/%D0%9A%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B/%D0%B1%D0%B8%D1%82%D0%BA%D0%BE%D0%B8%D0%BD/rub',
+            'urlDonor' => 'https://www.coingecko.com/en/coins/bitcoin/rub',
             'DOM' => '.col-lg-4 span[data-target="price.price"]'
          ];
       }
